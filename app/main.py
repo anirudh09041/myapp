069241562a32c8bbb57bf900ade94d86d9ca5fdf
Anirudh Kaushal <anirudh09041@gmail.com>
2025-11-26 18:41:48 +0000

    second commit

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from MyApp! Deployed via GitOps 🎉"
+    return "Hello from MyApp! Deployed via GitHub Actions and ArgoCD 🎉"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
